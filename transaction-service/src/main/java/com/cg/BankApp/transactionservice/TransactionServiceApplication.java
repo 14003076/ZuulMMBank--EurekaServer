13fe27c77771b67469de1c11d1d2b57d3935c462
Shaik <shaikshabzan@gmail.com>
2019-01-25 13:09:52 +0530

    adds mmbank using eureka server and zuul

---
+++ transaction-service/src/main/java/com/cg/BankApp/transactionservice/TransactionServiceApplication.java
@@ -0,0 +1,22 @@
+package com.cg.BankApp.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+}
+
